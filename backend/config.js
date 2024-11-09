091cff29368fd0db8f8b49825aeea2b28a0c5f71
pranav1310 <177325775+pranav1310@users.noreply.github.com>
2024-11-09 14:01:20 +0530

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://pranavrajendran60:pByI9ZvLHZ9Bt0Iu@book-store-mern.5zrdm.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN';
+
